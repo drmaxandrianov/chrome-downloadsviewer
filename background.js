d324fd2fc7666b1417bbd376b03735f2a69b7de1
Maxim Pestun <max.pestun@gmail.com>
2012-11-30 21:48:54 +0400

    try to send data about downloads

--- background.js
+++ background.js
@@ -1,7 +1,25 @@
 chrome.downloads.onCreated.addListener(function(downloadItem) {
-	$.get("http://pusher.tools.pixelowner.com", { 
-		id: downloadItem.id,  
-		filename: downloadItem.filename,  
+	$.post("http://downloads.pixelowner.com/ajax_add_downloads.php", { 
+		download_id: downloadItem.id,  
+		is_start: "true",  
+		is_end: "false",  
+		is_canceled: "false",  
 		url: downloadItem.url,  
+		name: downloadItem.url.substring(downloadItem.url.lastIndexOf("/")),  
+		pc_hash: localStorage.pc_hash,  
+		progress: 0 
 	} );
 });
+
+chrome.downloads.onChanged.addListener(function(downloadDelta) {
+	$.post("http://downloads.pixelowner.com/ajax_add_downloads.php", { 
+		download_id: downloadDelta.id,  
+		is_start: "false",  
+		is_end: "false",  
+		is_canceled: "false",  
+		url: "",
+		name: "",
+		pc_hash: localStorage.pc_hash,  
+		progress: downloadDelta.totalBytes 
+	} );
+} );
